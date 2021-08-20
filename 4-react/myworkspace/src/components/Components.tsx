aa6bfeb48e26c9a5b56c2695903ba2d7bac41fe8
Parkjinseok <87608033+jintolPark@users.noreply.github.com>
2021-08-20 17:56:01 +0900

    Add2

--- 4-react/myworkspace/src/components/Components.tsx
+++ 4-react/myworkspace/src/components/Components.tsx
@@ -1,10 +1,14 @@
-import Button from "../components/Button";
-import Header from "../components/Header";
-
+import Header from "./Header";
+import Button from "./Button";
 
 const Components = () => {
   return (
-    <div>    {/* 속성값을 변경하여 재사용하는 컴포넌트 */}
+    <div>
+      {/* JSX 내부에서 주석 달기 */}
+      {/* 재사용하지 않는 컴포넌트 */}
+      {/* <h1 style={{ color: "red" }}>Hello React with Typescript !</h1> */}
+
+      {/* 속성값을 변경하여 재사용하는 컴포넌트 */}
       {/* Component의 속성(prop)을 넘김 */}
       {/* 속성명={속성값} */}
       <Header color={"red"} title={"React"} />
@@ -13,13 +17,9 @@ const Components = () => {
 
       <Button variant={"primary"} text={"Add"} />
       <Button variant={"secondary"} text={"Delete"} />
-      <Button variant={"warning"} text={"Done"} />
-      {/* <Button color={"white"} backgroundColor={"blue"} text={"Delete"} />
-      <Button color={"white"} backgroundColor={"green"} text={"Function"} /> */}
+      <Button variant={"warning"} text={"Delete"} />
     </div>
-  )
-
-
-}
+  );
+};
 
 export default Components;
