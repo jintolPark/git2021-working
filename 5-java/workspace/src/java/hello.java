0890ff3254750e173adc988381a45971f9107a5b
Parkjinseok <87608033+jintolPark@users.noreply.github.com>
2021-09-06 11:07:48 +0900

    20210906java.

---
+++ 5-java/workspace/src/java/hello.java
@@ -0,0 +1,16 @@
+package java;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+
+		// Á¤¼öÇü (-2123123...0 +23233123, 4byte)
+		int x = 1;
+		int y = 2;
+		int result = x + y;
+		System.out.println(result);
+	}
+
+}
