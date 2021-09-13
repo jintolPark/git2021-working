d76d078666e3690ea4ef81902d5345d23db861e4
Park jin seok <pjsjja458@naver.com>
2021-09-13 09:32:41 +0900

    javaAccount

--- 5-java/workspace/15.collection/src/exam/BankApplication.java
+++ 5-java/workspace/15.collection/src/exam/BankApplication.java
@@ -6,16 +6,6 @@ import java.util.Scanner;
 
 public class BankApplication {
 
-	// Map 여러가지 형태의 Map 가능한 타입(Inteface)
-	// = HashMap
-	// = HashTable
-	// = TreeMap
-	//
-	// 대입하는 자료구조에 따라서 같은 메서드를 호출하더라도
-	// 내부적인 처리방식이 다름
-
-	// 계좌목록 Map 객체
-	// Map<키타입, 값타입> 변수명 = new HashMap<키타입, 값타입>();
 	private static Map<String, Account> accounts = new HashMap<String, Account>();
 
 	private static Scanner scanner = new Scanner(System.in);
@@ -47,18 +37,89 @@ public class BankApplication {
 
 	// 계좌생성하기(계좌추가하기)
 	private static void createAccount() {
+		System.out.println("----------");
+		System.out.println("계좌 생성");
+		System.out.println("----------");
 
+		System.out.println("----------");
+		System.out.println("계좌 번호 :");
+		String ano = scanner.next();
+
+		System.out.println("----------");
+		System.out.println("계좌 주인 :");
+		String owner = scanner.next();
+
+		System.out.println("----------");
+		System.out.println("초기 입금액 :");
+		int balance = scanner.nextInt();
+
+//		accounts.put(ano, new Account(ano, owner, balance));
+		Account a = new Account(ano, owner, balance);
+		accounts.put(ano, a);
+
+		System.out.println("----------");
+		System.out.println("계좌 생성 완료");
 	}
 
 	// 계좌목록보기
 	private static void accountList() {
+		System.out.println("-----------");
+		System.out.println("계좌 목록 조회");
+		System.out.println("-----------");
+		for (String ano : accounts.keySet()) {
+			String owner = accounts.get(ano).getOwner();
+			int balance = accounts.get(ano).getBalance();
+
+			System.out.println("계좌번호: " + ano + " 이름: " + owner + " 잔액: " + balance);
+		}
 	}
 
 	// 예금하기(필드값수정)
 	private static void deposit() {
+		System.out.println("-----------");
+		System.out.println("예금");
+		System.out.println("-----------");
+
+		System.out.println("-----------");
+		System.out.println("계좌번호 입력");
+
+		String ano = scanner.next();
+		if (accounts.containsKey(ano)) {
+			System.out.println("-----------");
+			System.out.println("예금액 입력");
+			int money = scanner.nextInt();
+			Account b = accounts.get(ano);
+			b.setBalance(b.getBalance() + money);
+			System.out.println(money + "원이 입금 되었습니다.");
+		} else {
+			System.out.println("계좌가 존재하지 않습니다.");
+		}
+
 	}
 
 	// 출금하기(필드값수정)
 	private static void withdraw() {
+		System.out.println("-----------");
+		System.out.println("출금");
+		System.out.println("-----------");
+
+		System.out.println("-----------");
+		System.out.println("계좌번호 입력");
+
+		String ano = scanner.next();
+		if (accounts.containsKey(ano)) {
+			System.out.println("-----------");
+			System.out.println("출금액 입력");
+			int money = scanner.nextInt();
+			Account b = accounts.get(ano);
+			if ((b.getBalance() - money) >= 0) {
+				b.setBalance(b.getBalance() - money);
+				System.out.println(money + "원이 출금 되었습니다.");
+			} else {
+				System.out.println("잔액이 부족합니다.");
+			}
+		} else {
+			System.out.println("계좌가 존재하지 않습니다.");
+		}
 	}
 }
